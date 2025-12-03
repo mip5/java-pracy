ba78894a61f68638de723a3f542b4dd3b7c0a962
mip5 <54771155+mip5@users.noreply.github.com>
2025-12-03 13:28:24 +0000

    code code code

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+class DataTypeExamples{
+
+    int myInt = 32;
+    double myDouble = 182.32;
+    char myChar = 'Q';
+    boolean myBoolean = false; 
+
+}
+public class DataTypes {
+    public static void main(String[] args){
+
+    DataTypeExamples dt = new DataTypeExamples();
+
+    System.out.println("My INT:" + dt.myInt);
+
+
+    }
+}
